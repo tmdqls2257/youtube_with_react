20a79ef1af326cb4769198cce4ede80003455b31
tmdqls2257 <tmdqls2257@naver.com>
2021-07-13 16:45:39 +0900

    video list

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import './app.css';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import Header from './components/header';
-import Video from './components/video';
+import Videolist from './components/video/videolist';
 import Nav from './components/nav';
 
 
-
 function App() {
+  const [videos, setVideos] = useState([]);
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyACklw79EzAcgQf9HuB4rJJYDsHHpBs-Ik&part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=25&regionCode=US&key=AIzaSyACklw79EzAcgQf9HuB4rJJYDsHHpBs-Ik", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
   return (
     <>
     <Header />
     <section className='content'>
     <Nav />
-    <Video />
+    <Videolist videos={videos}/>
     </section>
     </>
   );
-
 }
 
 export default App;
